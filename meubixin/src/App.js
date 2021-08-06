ea5576f83365faba1249801cc3cd9531d5c46c4d
arlegs <86801815+arlegs@users.noreply.github.com>
2021-08-05 21:00:27 -0300

    tela de login criada

---
+++ meubixin/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+import { PaginaLogin } from './components/pages/PaginaLogin/PaginaLogin';
+
+function App() {
+  return (
+   <PaginaLogin></PaginaLogin>
+  );
+}
+
+export default App;
---
+++ meubixin/src/components/pages/PaginaLogin/PaginaLogin.js
@@ -0,0 +1,26 @@
+import "./PaginaLogin.css";
+import logo from './logo.svg';
+import img from './img.png';
+
+export function PaginaLogin(){
+    return <div className="login">
+            <div className="esquerdo">
+            <img src={img} className="img_esquerda"/>
+            </div>
+            <div className="direito">
+                <img src={logo} className="img_logo"/>
+                <div className="formulario">
+                    <h1>Login</h1>
+                    <h2>Não perca a possibilidade de ter um novo<br></br> amiguinho ou de alegrar a vida dos que já tem!</h2>
+                    <div className="campos">
+                        <input placeholder="Digite seu email"></input>
+                        <input placeholder="Digite sua senha" type="password" className="senha"></input>               
+                        <h2>Esqueceu sua senha?</h2>
+                    </div>
+                    <button>Entrar</button>
+                   
+                </div>
+                
+            </div>
+    </div>
+}
